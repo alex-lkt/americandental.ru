c574eae699620e4b68106ecff5787f9f7cfa1a7c
/usr/bin/vim <support@beget.ru>
2023-08-18 22:13:45 +0300

    copy 2023-08-18

--- en/all-on-4/index.php
+++ en/all-on-4/index.php
@@ -1,22 +1,25 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-$APPLICATION->SetTitle("All on-4 implant placement surgery in Moscow Russia Europe");
-$APPLICATION->SetPageProperty("title", "All-on-4 implants  - \$2500 price in Moscow Russia Europe");
+$APPLICATION->SetPageProperty("title", "All-on-4 implantation - turnkey price in Moscow");
 require($_SERVER["DOCUMENT_ROOT"].SITE_TEMPLATE_PATH."/fullwidth-header.php");
 $APPLICATION->SetPageProperty("canonical", "https://americandental.ru/en/all-on-4/");
-$APPLICATION->SetPageProperty("description", "All-on-4 implants in Moscow Russia Europe with a permanent prosthesis at the American Dental Implantation Center Moscow. The official Nobel Biocare Center. From 120,000 rubles, warranty");
-?><section style="margin-top:50px;margin-bottom: 35px;">
-<div class="container">
-	<h1 style="margin-top:30px; margin-bottom:30px;">What is All on 4 “implants on four implants” or new teeth in 1 day?<br>
- </h1>
-</div>
- </section>
+$APPLICATION->SetPageProperty("description", "All-on-4 implantation with a permanent prosthesis at the American Dental Implantation Center Moscow. The official Nobel Biocare Center. From 120,000 rubles, warranty");
+?>
+
+
+<section style="margin-top:50px;margin-bottom: 35px;">
+	<div class="container">
+		<h1 style="margin-top:30px; margin-bottom:30px;">What is “implantation on four implants” or new teeth in 1 day?<br>
+	 </h1>
+	</div>
+</section>
+
 <div class="inner__gray" style="margin-top: 40px; margin-bottom:40px;">
 	<div class="container">
 		<div class="row align-items-center">
 			<div class="col-12 col-md-7 inner__header__text" style="margin-bottom:15px; margin-top:15px;">
 				<p class="hdr">
- <b>Immediate implants All-on-4 - price from 139.000 rub.</b>
+ <b>Immediate implantation All-on-4 - price from 139.000 rub.</b>
 				</p>
 				<p class="hdr">
  <b>Lifetime warranty with full personal responsibility of the doctor.</b>
@@ -29,35 +32,33 @@ $APPLICATION->SetPageProperty("description", "All-on-4 implants in Moscow Russia
 		</div>
 	</div>
 </div>
- <section>
-<div class="container">
-	<p>
-		 All-on-4 prosthetics is rehabilitation on four dental implants.<br>
-		 Two of them are placed vertically in the frontal part of the bone tissue, two others - in the distal or lateral part at an angle of up to 45º.<br>
-		 This allows you to abandon bone grafting in patients with atrophy or resorption of the jawbone.<br>
-		 The inclined installation gives the doctor ample opportunity to fix the implants - in the denser and deeper parts of the skull with a sufficient amount of bone, such as the zygomatic.
-	</p>
-	<p>
-		 On the upper jaw, the complex can be supplemented with zygomatic implants of the same brand - the Nobel Zygoma model.<br>
-		 They are fixed behind the jawbone - in the cheekbones, bypassing the nasal sinuses or with their partial displacement.<br>
-		 As a result, it is possible to restore a complete dentition even in conditions of pronounced atrophy of the maxillary bone without the need for sinus lift.
-	</p>
-	<h2 style="margin-top:30px; margin-bottom:30px;">All-on-4 implant surgery and prorthesis&nbsp;</h2>
-	<p>
-		 All-on-4 implant surgery and All on 4 restoration in our center in Moscow is carried out by a highly qualified American specialist, Dr. Soyfer. <br>
-		 Vladimir &nbsp;Soyfer has more than 25 years of clinical dental experience, confirmed by Nobel Branemark System USA certificates.
-	</p>
-	<h2 style="margin-top:30px; margin-bottom:30px;">Implantation in 1 day: what is All-on-4</h2>
-	<div class="text-center" style="margin-bottom:35px; margin-top:35px;">
- <img alt="Implantation in 1 day: what is All-on-4" src="/images/vidy-ustanovki-vse-na-4.png" title="Implantation in 1 day: what is All-on-4">
+
+<section>
+	<div class="container">
+		
+		<p>
+			 All-in-four prosthetics is rehabilitation on four dental implants.<br> Two of them are placed vertically in the frontal part of the bone tissue, two others - in the distal or lateral part at an angle of up to 45º.<br> This allows you to abandon bone grafting in patients with atrophy or resorption of the jawbone.<br> The inclined installation gives the doctor ample opportunity to fix the implants - in the denser and deeper parts of the skull with a sufficient amount of bone, such as the zygomatic.
+		</p>
+		
+		<p>
+			 On the upper jaw, the complex can be supplemented with zygomatic implants of the same brand - the Nobel Zygoma model.<br> They are fixed behind the jawbone - in the cheekbones, bypassing the nasal sinuses or with their partial displacement.<br> As a result, it is possible to restore a complete dentition even in conditions of pronounced atrophy of the maxillary bone without the need for sinus lift.
+		</p>
+		<h2 style="margin-top:30px; margin-bottom:30px;">All-on-4 implantation</h2>
+		
+		<p>
+			 All-on-4 implantation in our center in Moscow is carried out by a highly qualified American specialist, Dr. Soyfer. <br>Vladimir Valerievich Soyfer has more than 25 years of clinical dental experience, confirmed by Nobel Branemark System USA certificates.
+		</p>
+		
+		<h2 style="margin-top:30px; margin-bottom:30px;">Implantation in 1 day: what is All-on-4</h2>
+		<div class="text-center" style="margin-bottom:35px; margin-top:35px;">
+	 <img alt="Implantation in 1 day: what is All-on-4" title="Implantation in 1 day: what is All-on-4" src="/images/vidy-ustanovki-vse-na-4.png">
+		</div>
+		<p>
+			 The technology provides for the installation of a fixed bridge with a support for 4 implants, which at the same time serve as a support for the remaining "teeth".<br> The procedure involves the installation of implants for patients with a complete absence of teeth, or for patients with severely damaged teeth that must be removed.<br> Prosthetics are performed on the same day after the end of the tooth restoration operation in 1 day.
+		</p>
 	</div>
-	<p>
-		 The technology provides for the installation of a fixed bridge with a support for 4 implants, which at the same time serve as a support for the remaining "teeth".<br>
-		 The procedure involves the installation of implants for patients with a complete absence of teeth, or for patients with severely damaged teeth that must be removed.<br>
-		 Prosthetics are performed on the same day after the end of the tooth restoration operation in 1 day.
-	</p>
-</div>
- </section>
+</section>
+
 <?/*
 <div class="page4x4__offer">
 	<div class="container">
@@ -124,53 +125,53 @@ $APPLICATION->SetPageProperty("description", "All-on-4 implants in Moscow Russia
 		</div>
 	</div>
 </div>
-*/?> <section>
-<div class="container">
-	<h3 style="margin-top:30px; margin-bottom:30px;">A bit about the All-on-4 technology</h3>
-	<div style="margin-bottom:40px; margin-top:40px;text-align: center;">
- <img alt="A bit about the All-on-4 technology" src="/upload/medialibrary/eea/eea6930a11314826640b894d2e8650b3.jpg" title="A bit about the All-on-4 technology" style="max-width: 350px;">
+*/?>
+
+<section>
+	<div class="container">
+		<h3 style="margin-top:30px; margin-bottom:30px;">A bit about the All-on-4 technology</h3>
+        <div style="margin-bottom:40px; margin-top:40px;text-align: center;">
+	 <img src="/upload/medialibrary/eea/eea6930a11314826640b894d2e8650b3.jpg" alt="A bit about the All-on-4 technology" title="A bit about the All-on-4 technology" style="max-width: 350px;">
+     </div>
+     <div style="clear:both"></div>
+		<p>
+			 This implant technology was first conducted by Nobel Biocare.<br> The technique is effective and affordable from a financial point of view (in comparison with other implantation protocols).<br> The reliability and safety of the All-on-4 technology has been proven in numerous studies by the Portuguese dentist Paolo Malo.<br> A defining aspect of successful implantation is the study of bone structure and its compliance with established requirements. The study is carried out using cone beam computed tomography (CBCT).
+		</p>
+		<p>
+			 Key features of the All-on-Four implant technology:<br>
+		</p>
+		<ul>
+			<li>The support for the fixed bridge structure is 4 implants;</li>
+			<li>In order to prevent damage to the anatomical structures of the jaw, implants should be installed in the posterior part of the jaw at an appropriate angle.<br> Otherwise, the maxillary sinuses and the mandibular nerve may be damaged;</li>
+			<li>Upon completion of the operation, an immediate load is possible.</li>
+		</ul>
 	</div>
-	<div style="clear:both">
+</section>
+
+<section>
+	<div class="mt-4 mb-4" style="margin-bottom:15px; margin-top:15px;">
+		 <?$APPLICATION->IncludeFile($APPLICATION->GetTemplatePath("include-areas/inc.block-doctors.php"), array(), array("MODE" => "html"));?>
 	</div>
-	<p>
-		 This implant technology was first conducted by Nobel Biocare.<br>
-		 The technique is effective and affordable from a financial point of view (in comparison with other implantation protocols).<br>
-		 The reliability and safety of the All-on-4 technology has been proven in numerous studies by the Portuguese dentist Paolo Malo.<br>
-		 A defining aspect of successful implantation is the study of bone structure and its compliance with established requirements. The study is carried out using cone beam computed tomography (CBCT).
-	</p>
-	<p>
-		 Key features of the All-on-Four implant technology:<br>
-	</p>
-	<ul>
-		<li>The support for the fixed bridge structure is 4 implants;</li>
-		<li>In order to prevent damage to the anatomical structures of the jaw, implants should be installed in the posterior part of the jaw at an appropriate angle.<br>
-		 Otherwise, the maxillary sinuses and the mandibular nerve may be damaged;</li>
-		<li>Upon completion of the operation, an immediate load is possible.</li>
-	</ul>
-</div>
- </section> <section>
-<div class="mt-4 mb-4" style="margin-bottom:15px; margin-top:15px;">
-	 <?$APPLICATION->IncludeFile($APPLICATION->GetTemplatePath("include-areas/inc.block-doctors.php"), array(), array("MODE" => "html"));?>
-</div>
- </section> <section>
-<div class="container">
-	<p>
-		 The main advantages of the All-on-4 technology:
-	</p>
-	<ul>
-		<li>You can perform simultaneous prosthetics of implants of the entire dentition in one day, saving a considerable amount</li>
-		<li>Implantation will take much less time compared to other technologies.</li>
-		<li>The risk of further bone loss is significantly reduced.</li>
-		<li>Teeth will look perfect</li>
-		<li>You can correct the bite by applying oblique type abutments. The dentition will look even</li>
-	</ul>
-	<h3 style="margin-top:30px; margin-bottom:30px;">When is it appropriate to use "All-on-4"</h3>
-	<p>
-		 Loss of a tooth can often be accompanied by a loss of bone tissue, which places an urgent need for bone grafting to restore former volumes.<br>
-		 A distinctive feature of the All-for-Four technology is the possibility of multidirectional position of the axes of the front and rear implants.<br>
-		 Thus, the front implants are installed in the front of the jaw in parallel with the axes of the teeth, while the rear implants must be installed in the back of the jaw at an angle not exceeding 45 degrees relative to the axis of the teeth.<br>
-		 Thus, this protocol avoids damage to the maxillary sinus and mandibular canal without prior intervention of bone grafting.<br>
-		 Excellent primary stabilization of the prosthesis is achieved by increasing the area between the implant and the bone due to the inclination of the lateral implants, so the prosthetics procedure can be performed immediately after surgery. Please come to our center for All-on-4 implants here in Europe.
-	</p>
-</div>
- </section><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+</section>
+
+<section>
+	<div class="container">
+		<p>
+			 The main advantages of the All-on-4 technology:
+		</p>
+		<ul>
+			<li>You can perform simultaneous prosthetics of implants of the entire dentition in one day, saving a considerable amount</li>
+			<li>Implantation will take much less time compared to other technologies.</li>
+			<li>The risk of further bone loss is significantly reduced.</li>
+			<li>Teeth will look perfect</li>
+			<li>You can correct the bite by applying oblique type abutments. The dentition will look even</li>
+		</ul>
+		<h3 style="margin-top:30px; margin-bottom:30px;">When is it appropriate to use "All-on-4"</h3>
+		<p>
+			 Loss of a tooth can often be accompanied by a loss of bone tissue, which places an urgent need for bone grafting to restore former volumes.<br> A distinctive feature of the All-for-Four technology is the possibility of multidirectional position of the axes of the front and rear implants.<br> Thus, the front implants are installed in the front of the jaw in parallel with the axes of the teeth, while the rear implants must be installed in the back of the jaw at an angle not exceeding 45 degrees relative to the axis of the teeth.<br> Thus, this protocol avoids damage to the maxillary sinus and mandibular canal without prior intervention of bone grafting.<br> Excellent primary stabilization of the prosthesis is achieved by increasing the area between the implant and the bone due to the inclination of the lateral implants, so the prosthetics procedure can be performed immediately after surgery.
+		</p>
+		
+	</div>
+</section>
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
